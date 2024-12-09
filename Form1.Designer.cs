a25b667ce30742a3d7c9abfe073c263bfd422930
Zaki Omer <zakiomer624@gmail.com>
2024-12-09 17:20:58 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace LapThree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.lblChangedColorText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(181)))), ((int)(((byte)(56)))));
+            this.lblTitle.Location = new System.Drawing.Point(283, 34);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(223, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Section One Lap 3";
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Location = new System.Drawing.Point(252, 358);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(282, 61);
+            this.btnMessageBox.TabIndex = 1;
+            this.btnMessageBox.Text = "Show MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // lblChangedColorText
+            // 
+            this.lblChangedColorText.AutoSize = true;
+            this.lblChangedColorText.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChangedColorText.Location = new System.Drawing.Point(263, 195);
+            this.lblChangedColorText.Name = "lblChangedColorText";
+            this.lblChangedColorText.Size = new System.Drawing.Size(243, 27);
+            this.lblChangedColorText.TabIndex = 2;
+            this.lblChangedColorText.Text = "Changed Text To Blue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Changed Text To Blue";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Changed Text To Blue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(510, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Changed Text To Blue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(520, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 27);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Changed Text To Blue";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(261, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 27);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Changed Text To Blue";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 27);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Changed Text To Blue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblChangedColorText);
+            this.Controls.Add(this.btnMessageBox);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label lblChangedColorText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LapThree
+{
+    public partial class Form1 : Form
+    {
+        //Create Vars in Here!
+        int Num = 23;
+        double DbNum = 9.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            //int Result = Num;
+            //MessageBox.Show("The Number Stored in The Var is " + Result.ToString());
+            //MessageBox.Show("");
+
+            //2 Var -> ADD, SUBS, MULTI, DIV
+            //RESULT MESSAGE
+
+            //Change Colors when button is clicked
+            //1. Directly => name + . + propertyName = property value;
+            lblChangedColorText.ForeColor = Color.Red;
+
+            //Abdiqaadir Ibrahim
+            //lblChangedColorText.Text = ''
+
+            //Abdimajid 
+            //lblChangedColorText.Text = Color("Abdimajid") == Mistake
+
+
+        }
+    }
+}
